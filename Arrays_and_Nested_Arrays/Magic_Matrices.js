5c0e9a09ae504790d5bc1b76fecfe6797ba7e4d2
Rosen Ignatov <rosen.ignatoff@gmail.com>
2024-01-19 00:58:11 +0200

    exercise

--- Arrays_and_Nested_Arrays/Magic_Matrices.js
+++ Arrays_and_Nested_Arrays/Magic_Matrices.js
@@ -2,12 +2,11 @@ function magicMatrices(matrix) {
 
     for (let i = 0; i < matrix.length - 1; i++) {
 
-        let columnOne = matrix.reduce((acc, num) => acc + num[i], 0);
-        let columnTwo = matrix.reduce((acc, num) => acc + num[i + 1], 0);
-
         let rowOne = matrix[i].reduce((acc, num) => acc + num);
         let rowTwo = matrix[i + 1].reduce((acc, num) => acc + num);
-
+        let columnOne = matrix.reduce((acc, num) => acc + num[i], 0);
+        let columnTwo = matrix.reduce((acc, num) => acc + num[i + 1], 0);
+        
         if(rowOne != rowTwo || columnOne != columnTwo) {
             return false;
         }
