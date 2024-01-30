a229415e8d179d59faf0736c86dce9e725cff25c
Rosen Ignatov <rosen.ignatoff@gmail.com>
2024-01-30 23:27:35 +0200

    exercise

--- DOM_Manipulations_and_Events/Sections/script.js
+++ DOM_Manipulations_and_Events/Sections/script.js
@@ -1,25 +1,24 @@
 function create(words) {
+  const contentEl = document.getElementById("content");
 
-   const contentEl = document.getElementById("content");
+  for (let word of words) {
+    let divEl = document.createElement("div");
+    let pEl = document.createElement("p");
 
-   for (let word of words) {
-      let divEl = document.createElement('div');
-      let pEl = document.createElement('p');
+    pEl.textContent = word;
+    pEl.style.display = "none";
 
-      pEl.textContent = word;
-      pEl.style.display = 'none';
+    divEl.appendChild(pEl);
+    contentEl.appendChild(divEl);
 
-      divEl.appendChild(pEl);
-      contentEl.appendChild(divEl);
+    divEl.addEventListener("click", clickE);
+  }
 
-      divEl.addEventListener('click', clickE)
-   }
-
-   function clickE(event) {
-      let target = event.currentTarget;
-      let elChildren = target.children;
-      let paragraph = elChildren[0];
-      paragraph.style.display = 'block';
-      // /* if we want can make them switchable -> */ paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
-   }
+  function clickE(event) {
+    let target = event.currentTarget;
+    let elChildren = target.children;
+    let paragraph = elChildren[0];
+    paragraph.style.display = "block";
+    // /* if we want can make them switchable -> */ paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
 }
---
+++ DOM_Manipulations_and_Events/Time-Converter/solution.js
@@ -0,0 +1,30 @@
+function attachEventsListeners() {
+  const buttons = Array.from(document.querySelectorAll("input[type=button]"));
+
+  for (let btn of buttons) {
+    btn.addEventListener("click", converter);
+  }
+
+  function converter(event) {
+    let content = event.currentTarget.parentElement.children[1];
+    let input = Number(content.value);
+    let inputType = content.id;
+
+    if (inputType == "days") {
+      timeCalculator(input);
+    } else if (inputType == "hours") {
+      timeCalculator(input / 24);
+    } else if (inputType == "minutes") {
+      timeCalculator(input / 24 / 60);
+    } else if (inputType == "seconds") {
+      timeCalculator(input / 24 / 60 / 60);
+    }
+  }
+  function timeCalculator(input) {
+    let inputs = Array.from(document.querySelectorAll("input[type=text]"));
+    inputs[0].value = input;
+    inputs[1].value = input * 24;
+    inputs[2].value = input * 24 * 60;
+    inputs[3].value = input * 24 * 60 * 60;
+  }
+}
