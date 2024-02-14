d1b706c4f1950a930a399a55cb81a374d54c2e5f
Rosen Ignatov <rosen.ignatoff@gmail.com>
2024-02-14 23:01:28 +0200

    exercise

--- Exam-Preparation-One/01. Hotel reservation-Resources/app.js
+++ Exam-Preparation-One/01. Hotel reservation-Resources/app.js
@@ -11,9 +11,9 @@ function solve() {
   const confirmReservationEl = document.querySelector(".confirm-list");
   const verificationEl = document.querySelector("#verification");
 
-  const nextBtn = document.querySelector('#next-btn');
+  const nextBtn = document.querySelector("#next-btn");
 
-  nextBtn.addEventListener('click', addReservation);
+  nextBtn.addEventListener("click", addReservation);
 
   function addReservation(event) {
     event.preventDefault();
@@ -24,17 +24,23 @@ function solve() {
     let dateOut = dateOutEl.value;
     let peopleCount = peopleCountEl.value;
 
-    if (!firstName || !lastName || !dateIn || !dateOut|| !peopleCount) {
-        return;
+    if (!firstName || !lastName || !dateIn || !dateOut || !peopleCount) {
+      return;
     }
 
     reservationCreator(firstName, lastName, dateIn, dateOut, peopleCount);
   }
 
-  function reservationCreator(firstName, lastName, dateIn, dateOut, peopleCount) {
+  function reservationCreator(
+    firstName,
+    lastName,
+    dateIn,
+    dateOut,
+    peopleCount
+  ) {
     nextBtn.disabled = true;
 
-    const li = document.createElement('li')
+    const li = document.createElement("li");
     const article = document.createElement("article");
     const h3 = document.createElement("h3");
     const pOne = document.createElement("p");
@@ -47,12 +53,12 @@ function solve() {
     pOne.textContent = `From date: ${dateIn}`;
     pTwo.textContent = `To date: ${dateOut}`;
     pThree.textContent = `For ${peopleCount} people`;
-    btnOne.textContent = 'Edit';
-    btnTwo.textContent = 'Continue';
+    btnOne.textContent = "Edit";
+    btnTwo.textContent = "Continue";
 
-    li.classList.add('reservation-content');
-    btnOne.classList.add('edit-btn');
-    btnTwo.classList.add('continue-btn');
+    li.classList.add("reservation-content");
+    btnOne.classList.add("edit-btn");
+    btnTwo.classList.add("continue-btn");
 
     article.appendChild(h3);
     article.appendChild(pOne);
@@ -62,34 +68,44 @@ function solve() {
     li.appendChild(btnOne);
     li.appendChild(btnTwo);
 
-    firstNameEl.value = '';
-    lastNameEl.value = '';
-    dateInEl.value = '';
-    dateOutEl.value = '';
-    peopleCountEl.value = '';
+    firstNameEl.value = "";
+    lastNameEl.value = "";
+    dateInEl.value = "";
+    dateOutEl.value = "";
+    peopleCountEl.value = "";
 
     reservationInfoEl.appendChild(li);
 
-    btnOne.addEventListener('click', () => backToTheRoots(firstName, lastName, dateIn, dateOut, peopleCount));
-    btnTwo.addEventListener('click', () => confirmReservation(firstName, lastName, dateIn, dateOut, peopleCount));
+    btnOne.addEventListener("click", () =>
+      backToTheRoots(firstName, lastName, dateIn, dateOut, peopleCount)
+    );
+    btnTwo.addEventListener("click", () =>
+      confirmReservation(firstName, lastName, dateIn, dateOut, peopleCount)
+    );
   }
 
   function backToTheRoots(firstName, lastName, dateIn, dateOut, peopleCount) {
     nextBtn.disabled = false;
-    
+
     firstNameEl.value = firstName;
     lastNameEl.value = lastName;
     dateInEl.value = dateIn;
     dateOutEl.value = dateOut;
     peopleCountEl.value = peopleCount;
 
-    reservationInfoEl.textContent = '';
+    reservationInfoEl.textContent = "";
   }
 
-  function confirmReservation(firstName, lastName, dateIn, dateOut, peopleCount) {
-    reservationInfoEl.textContent = '';
+  function confirmReservation(
+    firstName,
+    lastName,
+    dateIn,
+    dateOut,
+    peopleCount
+  ) {
+    reservationInfoEl.textContent = "";
 
-    const li = document.createElement('li')
+    const li = document.createElement("li");
     const article = document.createElement("article");
     const h3 = document.createElement("h3");
     const pOne = document.createElement("p");
@@ -102,12 +118,12 @@ function solve() {
     pOne.textContent = `From date: ${dateIn}`;
     pTwo.textContent = `To date: ${dateOut}`;
     pThree.textContent = `For ${peopleCount} people`;
-    btnOne.textContent = 'Confirm';
-    btnTwo.textContent = 'Cancel';
+    btnOne.textContent = "Confirm";
+    btnTwo.textContent = "Cancel";
 
-    li.classList.add('reservation-content');
-    btnOne.classList.add('confirm-btn');
-    btnTwo.classList.add('cancel-btn');
+    li.classList.add("reservation-content");
+    btnOne.classList.add("confirm-btn");
+    btnTwo.classList.add("cancel-btn");
 
     article.appendChild(h3);
     article.appendChild(pOne);
@@ -119,18 +135,20 @@ function solve() {
 
     confirmReservationEl.appendChild(li);
 
-    btnOne.addEventListener('click', finish.bind(null, true));
-    btnTwo.addEventListener('click', finish.bind(null, false));
+    btnOne.addEventListener("click", finish.bind(null, true));
+    btnTwo.addEventListener("click", finish.bind(null, false));
   }
 
   function finish(confirmed) {
-    let className = confirmed ? "reservation-confirmed" : "reservation-cancelled";
+    let className = confirmed
+      ? "reservation-confirmed"
+      : "reservation-cancelled";
     let text = confirmed ? "Confirmed." : "Cancelled.";
 
     verificationEl.className = className;
     verificationEl.textContent = text;
 
-    confirmReservationEl.textContent = '';
+    confirmReservationEl.textContent = "";
     nextBtn.disabled = false;
   }
 }
