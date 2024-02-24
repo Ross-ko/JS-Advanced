9238957fb7efa6713811c741e04d57219e950e88
Rosen Ignatov <rosen.ignatoff@gmail.com>
2024-02-24 17:22:14 +0200

    exercise

--- Prototypes_and_Inheritance/Inheritance.js
+++ Prototypes_and_Inheritance/Inheritance.js
@@ -1,26 +1,25 @@
 class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;       
-    }
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-    sayHello() {
-        console.log(`${this.name} says hello!`);
-    }
+  sayHello() {
+    console.log(`${this.name} says hello!`);
+  }
 }
 
 class Student extends Person {
-    constructor(name, age, grade) {
-        super(name, age);
+  constructor(name, age, grade) {
+    super(name, age);
 
-        this.grade = grade;
-    }
+    this.grade = grade;
+  }
 }
 
-let person = new Student('Albert', 13, 'Seventh');
+let person = new Student("Albert", 13, "Seventh");
 console.log(person);
 person.sayHello();
 
 console.log(person instanceof Person);
 console.log(person.constructor);
-
